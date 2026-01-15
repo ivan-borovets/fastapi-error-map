e58a1380cabfb311d2eae46e0724557159af2581
vyuroshchin <vyuroshchin@sberautotech.ru>
2026-01-15 16:00:16 +0300

    run all versions tests in locally by default

--- noxfile.py
+++ noxfile.py
@@ -1,9 +1,19 @@
 import nox
 
+PYTHON_SUPPORT_VERSIONS = ("3.9", "3.10", "3.11", "3.12", "3.13", "3.14")
 
-@nox.session(python=None)
-@nox.parametrize("fastapi", ["0.100.0", "latest"])
+
+@nox.session(python=PYTHON_SUPPORT_VERSIONS)
+@nox.parametrize("fastapi", ("0.100.0", "latest"))
 def compatibility(session, fastapi):
+    """
+    Examples:
+        run all versions python and fastapi
+        nox
+
+        run python and fastapi versions
+        nox -s compatibility -p 3.14 -- fastapi=latest
+    """
     session.run("uv", "sync", "--locked", "--group", "test", "--active", external=True)
 
     if fastapi == "latest":
