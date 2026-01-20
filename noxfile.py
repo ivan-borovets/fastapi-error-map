e3254311880bb700a40ca14ba03f286bef468cc2
ivan-borovets <130386813+ivan-borovets@users.noreply.github.com>
2026-01-20 23:26:41 +0300

    Remove docstring

--- noxfile.py
+++ noxfile.py
@@ -6,14 +6,6 @@ PYTHON_SUPPORT_VERSIONS = ("3.9", "3.10", "3.11", "3.12", "3.13", "3.14")
 @nox.session(python=PYTHON_SUPPORT_VERSIONS)
 @nox.parametrize("fastapi", ("0.100.0", "latest"))
 def compatibility(session, fastapi):
-    """
-    Examples:
-        run all versions python and fastapi
-        nox
-
-        run python and fastapi versions
-        nox -s compatibility -p 3.14 -- fastapi=latest
-    """
     session.run("uv", "sync", "--locked", "--group", "test", "--active", external=True)
 
     if fastapi == "latest":
